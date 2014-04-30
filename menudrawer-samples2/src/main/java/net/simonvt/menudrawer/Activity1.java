cf383e17c551db3026a0a7937d2bd7127a95a214
destinyd <destinyd_war@163.com>
2014-04-30 12:51:34 +0800

    samples2

---
+++ menudrawer-samples2/src/main/java/net/simonvt/menudrawer/Activity1.java
@@ -0,0 +1,27 @@
+package net.simonvt.menudrawer;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.awt.*;
+
+public class Activity1 extends ActivityBase {
+
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMenuDrawer.setContentView(R.layout.activity1);
+    }
+}
+
---
+++ menudrawer-samples2/src/main/java/net/simonvt/menudrawer/Activity4.java
@@ -0,0 +1,20 @@
+package net.simonvt.menudrawer;
+
+import android.os.Bundle;
+
+public class Activity4 extends ActivityBase {
+
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMenuDrawer.setContentView(R.layout.activity4);
+    }
+}
+
