43e43d278aeb811f40f0fc56e6f320b92ed8725a
destinyd <destinyd_war@163.com>
2014-07-10 15:27:07 +0000

    change project structure

---
+++ menudrawer-samples/src/com/github/destinyd/menudrawer/samples/Fragment3.java
@@ -0,0 +1,17 @@
+package com.github.destinyd.menudrawer.samples;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dd on 14-5-4.
+ */
+public class Fragment3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity3, null);
+    }
+}
